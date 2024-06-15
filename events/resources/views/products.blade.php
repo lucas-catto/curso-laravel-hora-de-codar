3db040697ea9f0e33fe8295e72e0aa5257976701
lucas-catto <lucascattosilva2018@gmail.com>
2024-06-14 23:51:49 -0300

    Criando layout com blade - #07

--- events/resources/views/products.blade.php
+++ events/resources/views/products.blade.php
@@ -1,13 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Produtos</title>
-        <link rel="stylesheet" href="./assets/css/style.css">
-    </head>
-    <body>
-        <h1>Produtos</h1>
-    </body>
-</html>
+
+@extends('layouts.main')
+
+@section('title', 'Produtos')
+
+@section('content')
+
+<h1>Produtos</h1>
+
+@endsection
--- events/resources/views/welcome.blade.php
+++ events/resources/views/welcome.blade.php
@@ -1,66 +1,60 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+@extends('layouts.main')
 
-        <link rel="stylesheet" href="./assets/css/style.css">
-        <script src="./assets/js/script.js"></script>
-    </head>
-    <body>
-        <h1>Title</h1>
+@section('title', 'HDC Events')
 
-        <img src="./assets/img/banner.jpg" alt="">
+@section('content')
+    
+<h1>Title</h1>
 
-        @if (10 > 5)
-            <p>true</p>
-        @endif
+<img src="./assets/img/banner.jpg" alt="">
 
-        <p>{{ $name }}</p>
+@if (10 > 5)
+    <p>true</p>
+@endif
 
-        @if ($name == 'lucas')
-            <p>Hi {{ $name }}</p>
-        @else
-            <p>Hi</p>
-        @endif
+<p>{{ $name }}</p>
 
-        {{-- --}}
+@if ($name == 'lucas')
+    <p>Hi {{ $name }}</p>
+@else
+    <p>Hi</p>
+@endif
 
-        @if ($name == 'lucas')
-            <p>{{ $name }} is {{ $age }} years old.</p>
+{{-- --}}
 
-        @elseif ($name == 'taylor')
-            <p>{{ $name }}</p>
+@if ($name == 'lucas')
+    <p>{{ $name }} is {{ $age }} years old.</p>
 
-        @else
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+@elseif ($name == 'taylor')
+    <p>{{ $name }}</p>
 
-        @endif
+@else
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
 
-        {{-- --}}
+@endif
 
-        @for ($i = 0; $i < count($numbers); $i++)
-            @if ($i == 2)
-                <p>Hi {{ $i }}</p>
-            @else
-                <p>{{ $numbers[$i] }} - {{ $i }}</p>
-            @endif
-        @endfor
+{{-- --}}
 
-        {{-- --}}
+@for ($i = 0; $i < count($numbers); $i++)
+    @if ($i == 2)
+        <p>Hi {{ $i }}</p>
+    @else
+        <p>{{ $numbers[$i] }} - {{ $i }}</p>
+    @endif
+@endfor
 
-        @php
-            $laravel = 'framework';
-            echo $laravel;
-        @endphp
+{{-- --}}
 
-        {{-- --}}
+@php
+    $laravel = 'framework';
+    echo $laravel;
+@endphp
 
-        @foreach ($names as $name)
-            <p>{{ $loop->index }} - {{ $name }}</p>
-        @endforeach
+{{-- --}}
 
-    </body>
-</html>
+@foreach ($names as $name)
+    <p>{{ $loop->index }} - {{ $name }}</p>
+@endforeach
+
+@endsection
