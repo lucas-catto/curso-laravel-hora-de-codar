43227c02dfb93856a74e625bdf5820d51ef73f93
lucas-catto <lucascattosilva2018@gmail.com>
2024-06-21 23:45:26 -0300

    Avançando em migrations do Laravel - #12

--- events/database/migrations/2024_06_22_023743_add_category_to_products_table.php
+++ events/database/migrations/2024_06_22_023743_add_category_to_products_table.php
@@ -11,12 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
-            $table->id();
-            $table->string('name', 100);
-            $table->integer('qty');
-            $table->text('description');
-            $table->timestamps();
+        Schema::table('products', function (Blueprint $table) {
+            $table->string('category');
         });
     }
 
@@ -25,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('category');
+        });
     }
 };
