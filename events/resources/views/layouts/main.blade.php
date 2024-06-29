7966c35e4f9c27846eb59e7e14df44051b79c568
lucas-catto <lucascattosilva2018@gmail.com>
2024-06-29 17:53:42 -0300

    Salvando dados no banco MySQL - #15

--- events/resources/views/layouts/main.blade.php
+++ events/resources/views/layouts/main.blade.php
@@ -13,8 +13,8 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
         
         <!-- CSS da aplicação -->
-        <link rel="stylesheet" href="./assets/css/style.css">
-        <script src="./assets/js/script.js"></script>
+        <link rel="stylesheet" href="/assets/css/style.css">
+        <script src="/assets/js/script.js"></script>
     </head>
     <body>
         <header>
@@ -22,8 +22,9 @@
                 <div class="collapse navbar-collapse" id="navbar">
                     
                     <a href="/" class="navbar-brand">
-                        <img src="./assets/img/hdcevents_logo.svg" alt="HDC Events">
+                        <img src="/assets/img/hdcevents_logo.svg" alt="HDC Events">
                     </a>
+                    
                     <ul class="navbar-nav">
                         
                         <li class="nav-item">
